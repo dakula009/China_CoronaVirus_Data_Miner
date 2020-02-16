03123c6b056136cbbd0e9d65597aa464f4315f24
Ran L <45241071+dakula009@users.noreply.github.com>
2020-02-15 18:18:02 -0800

    Added country level data;  Added death rate and heal rate to all levels.

--- china_data_mining.py
+++ china_data_mining.py
@@ -22,11 +22,16 @@ data = json.loads(data['data'])
 lastUpdateTime = data['lastUpdateTime']
 print('数据更新时间 ' + str(lastUpdateTime))
 
+
+# part 1. 采集当日数据
 areaTree = data['areaTree']
 
+print('采集当日数据...')
+
 # 创建空 dataframes
-col_names =  ['省', '市', '新增确认','确认' , '死亡', '治愈']
-col_names_p =  ['省','确认', '新增确认',  '死亡', '治愈']
+col_names =  ['省', '市', '新增确诊','累计确诊', '死亡', '治愈','死亡率','治愈率']
+# col_names_p =  ['省','新增确诊', '累计确诊' '死亡', '治愈','死亡率','治愈率']
+col_names_p = ['省', '新增确诊', '累计确诊', '死亡', '治愈', '死亡率', '治愈率']
 
 my_df  = pd.DataFrame(columns = col_names)
 my_df_p = pd.DataFrame(columns = col_names_p)
@@ -44,9 +49,13 @@ for item in areaTree:
             death = item_p['total']['dead']
             heal = item_p['total']['heal']
             new_confirm = item_p['today']['confirm']
+            deadRate =item_p['total']['deadRate']
+            healRate =item_p['total']['healRate']
 
             # 向df添加数据
-            data_dict = {'省': province,'新增确认':new_confirm,'确认': confirm, '死亡': death, '治愈': heal}
+            data_dict = {'省': province,'新增确诊':new_confirm,'累计确诊': confirm,
+                         '死亡': death, '治愈': heal, '死亡率': deadRate, '治愈率': healRate}
+            # print (data_dict)
             my_df_p.loc[len(my_df_p)] = data_dict
 
             # 遍历地级数据
@@ -60,9 +69,12 @@ for item in areaTree:
                 # suspect = item_c['total']['suspect']
                 death = item_c['total']['dead']
                 heal = item_c['total']['heal']
+                deadRate = item_c['total']['deadRate']
+                healRate = item_c['total']['healRate']
 
                 # 向df添加数据
-                data_dict = {'省': province, '市':prefecture, '新增确认':new_confirm,'确认': confirm, '死亡': death, '治愈': heal}
+                data_dict = {'省': province, '市':prefecture, '新增确诊':new_confirm,'累计确诊': confirm,
+                             '死亡': death, '治愈': heal, '死亡率': deadRate, '治愈率': healRate}
                 my_df.loc[len(my_df)] = data_dict
 
 # 保存数据
@@ -70,5 +82,34 @@ my_df.index += 1   # 使index从1开始
 my_df_p.index += 1
 my_df.to_csv(r'./china_prefecture_status_{}.csv'.format(str(lastUpdateTime).split()[0]), encoding='utf_8_sig', header='true')
 my_df_p.to_csv(r'./china_province_status_{}.csv'.format(str(lastUpdateTime).split()[0]), encoding='utf_8_sig', header='true')
+
+# part 2. 采集中国历史数据
+
+print('采集全国历史数据...')
+
+china_day_list = data['chinaDayList']
+
+col_names_cd =  ['日期','累计确诊','疑似','死亡', '治愈', '现有确诊', '现有重症','死亡率','治愈率']
+
+my_df_cd = pd.DataFrame(columns = col_names_cd)
+
+for day_item in china_day_list:
+    date = day_item['date'] + '.2020'
+    confirm = day_item['confirm']
+    suspect = day_item['suspect']
+    dead = day_item['dead']
+    heal = day_item['heal']
+    nowConfirm = day_item['nowConfirm']
+    nowSevere = day_item['nowSevere']
+    deadRate = day_item['deadRate']
+    healRate = day_item['healRate']
+
+    # 向df添加数据
+    data_dict = {'日期': date,'累计确诊': confirm,'疑似': suspect,'死亡': dead, '治愈': heal, '现有确诊': nowConfirm,
+                 '现有重症':nowSevere,'死亡率': deadRate,'治愈率':healRate}
+    my_df_cd.loc[len(my_df_cd)] = data_dict
+
+my_df_cd.index += 1
+my_df_cd.to_csv(r'./china_daily_status_{}.csv'.format(str(lastUpdateTime).split()[0]), encoding='utf_8_sig', header='true')
 
 print('Success')
